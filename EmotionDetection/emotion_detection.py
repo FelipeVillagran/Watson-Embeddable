26b37f1ec3419d1ee5226abd34706f975bd512bc
FelipeVillagran <129631500+FelipeVillagran@users.noreply.github.com>
2024-01-26 16:23:04 -0300

    Recordatorio para ingresar API Key

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,7 +2,7 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def analyze_emotions(text_to_analyze):
-    api_key = "nKVoIRpFA2kz-EAbowr0tHRooqOUYM8Ju3TePOvYZYil"
+    api_key = "INSERTAR_API_KEY_SERVICIO_IBM"
     url = "https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/39e3a091-d296-4a3d-bfbd-bb73a4138323"
 
     authenticator = IAMAuthenticator(api_key)
